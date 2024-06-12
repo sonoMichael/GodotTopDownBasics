bc192ada35ea238038a7c9f38ebaf7179c8ece19
Michael Zhao (Beta80) <michael.zhao@beta80group.it>
2024-06-12 10:50:00 +0200

    Player Animation Walk
    Create Player Walking animation

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -2,18 +2,20 @@ extends CharacterBody2D
 
 
 const Speed = 75.0
+
 @onready var animationTree = $AnimationTree
 
 
 func movementInput():
-	var inputDirection = Input.get_vector("Left", "Right", "Up", "Down")
+	var inputDirection = Input.get_vector("Left", "Right", "Up", "Down").normalized()
 	velocity = inputDirection * Speed
-	animationTree.set("parameters/Idle/blend_position", inputDirection)
-	#var stateMachine = animationTree.get("parameters/playback")
-	#if (inputDirection != Vector2.ZERO):
-		#pass
-	#else:
-		#stateMachine.travel("Idle")
+	var stateMachine = animationTree["parameters/playback"]
+	if (inputDirection == Vector2.ZERO):
+		stateMachine.travel("Idle")
+	else:
+		stateMachine.travel("Walk")
+		animationTree.set("parameters/Idle/blend_position", inputDirection)
+		animationTree.set("parameters/Walk/blend_position", inputDirection)
 
 func _physics_process(delta):
 	movementInput()
